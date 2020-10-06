ee25a85d8294c4666545e4a4320309037b73c6d8
Andrew Cliffe <acliffe45@gmail.com>
2020-10-05 19:44:18 -0700

    updating files 10-5-2020 1944

---
+++ app.py
@@ -0,0 +1,35 @@
+import requests
+import json
+from flask import Flask, render_template, redirect, jsonify
+from flask_pymongo import PyMongo
+
+from bson import json_util
+from bson.objectid import ObjectId
+
+
+url = "https://api.weather.gov/alerts/active?area=CA"
+ca = requests.get(url).json()
+
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/firezones"
+mongo = PyMongo(app)
+
+db = mongo.db.zones
+
+db.zones.drop()
+
+db.zones.insert_one(ca)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/data", methods = ['GET'])
+def index():
+    data = list(db.zones.find())
+    return json.dumps(data, default=json_util.default)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ assets/js/test.ipynb
@@ -0,0 +1,15 @@
+import requests
+import json
+
+url = "https://api.weather.gov/alerts/active?area="
+
+CA = requests.get(url + "CA").json()
+features = (CA["features"])
+activezone = []
+for row in features:
+
+    zones = (row["properties"]["affectedZones"])
+    for zone in zones:
+        activezone.append(zone)
+
+print(activezone)
