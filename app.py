2914e178b2583bcd2c5e37e33dedad62dfad049e
James <jamess2eagle@gmail.com>
2020-10-08 21:09:15 -0700

    added layers

--- app.py
+++ app.py
@@ -5,22 +5,32 @@ from flask_pymongo import PyMongo
 
 from bson import json_util
 from bson.objectid import ObjectId
+import geojson
+from bson.json_util import loads
 
 
-url = "https://api.weather.gov/alerts/active?area=CA"
+url = "https://api.weather.gov/alerts/active"
 ca = requests.get(url).json()
-
-
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/firezones"
 mongo = PyMongo(app)
-
 db = mongo.db.zones
-
 db.zones.drop()
-
 db.zones.insert_one(ca)
 
+# db2 = mongo.db.fires
+
+with open("data.geojson") as f:
+  gj = geojson.load(f)
+
+
+# calling a file
+# with open("data.geojson") as f:
+#     gj = geojson.load(f)
+# print(gj)
+
+# db2.fires.drop()
+# db2.fires.insert_one(gj)
 
 @app.route("/")
 def home():
@@ -30,6 +40,12 @@ def home():
 def index():
     data = list(db.zones.find())
     return json.dumps(data, default=json_util.default)
+
+@app.route("/data2", methods = ['GET'])
+def index2():
+    return gj
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/appv7.js
+++ static/js/appv7.js
@@ -1,103 +1,126 @@
 url = "http://127.0.0.1:5000/data";
+url2 = "http://127.0.0.1:5000/data2";
 
-var myMap = L.map("map", {
-    center: [34.0522, -118.2437],
-    zoom: 8
+
+
+function insertMap(alertLayer, fireLayer) {
+
+  // Adding tile layer
+  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
   });
+  var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "dark-v10",
+    accessToken: API_KEY
+  });
+  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "light-v10",
+    accessToken: API_KEY
+  });
+  var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "satellite-v9",
+    accessToken: API_KEY
+  });
+  var outdoor = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "outdoors-v11",
+    accessToken: API_KEY
+  });
+  //set base layers
+  var baseMaps = {
+    "Street Map": streetmap,
+    "Dark Map": darkmap,
+    "Light Map": lightmap,
+    "Satellite Map": satellite,
+    "Outdoor Map": outdoor
+  };
+  //set overlay maps
+  var overlayMaps = {
+    "Alerts": alertLayer,
+    "Active Fire": fireLayer
+  };
+  //initial map settings
+  var myMap = L.map("map", {
+      center: [37.09, -75.71],
+      center: [53.5, -98.35],
+      zoom:4,
+      maxZoom: 7,
+      layers: [streetmap,fireLayer,alertLayer]
+  });
+  //add clickable layers
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
 
-// Adding tile layer
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: API_KEY
-}).addTo(myMap);
+}
 
-
-zones = [];
-type = [];
-lat = [];
-lon = [];
 
 d3.json(url).then(data => {
-    console.log(data);
-    data2 = data[0].features;
-    console.log(data2);
-    data2.forEach((row, i) => {
-        activeZones = row.properties.affectedZones;
-        // console.log(affectedZones);
-        activeZones.forEach((row2, j) => {
-            // console.log(`Active Fire: ${i}, Affected Zones: ${j} ${fires}`);
-            // console.log(row2);
-            zones.push(row2);
-            console.log(row2);
+  data2 = data[0].features;
+  data2.forEach(d => {
+      properties = d.properties
+      activeZones = properties.affectedZones
+      onset = properties.onset
+      status = properties.status
+      urgency = properties.urgency
+      eventtype = properties.event
+      severity = properties.severity
+      headline = properties.headline
+      description = properties.description
+      instruction = properties.instruction
+      parameters = properties.parameters
+      console.log(eventtype)
+      
 
-            d3.json(row2).then(datalatlon => {
-            /*    geojson = L.choropleth(datalatlon), {
-                     // Define what  property in the features to use
-                    valueProperty: "MHI2016",
-
-                    // Set color scale
-                    scale: ["#ffffb2", "#b10026"],
-
-                    // Number of breaks in step range
-                    steps: 10,
-
-                    // q for quartile, e for equidistant, k for k-means
-                    mode: "q",
-                    style: {
-                    // Border color
-                    color: "#fff",
-                    weight: 1,
-                    fillOpacity: 0.8
-                    },
-
-                    // Binding a pop-up to each layer
-                    onEachFeature: function(feature, layer) {
-                    layer.bindPopup("test725");
-                    }
-                }.addTo(myMap);        
-            */
-                    console.log(datalatlon);
-        }
-            )
-            
+    activeZones.forEach(row2=>{
+      d3.json(row2).then(datalatlon =>{
+        var alertLayer = L.geoJson(datalatlon,{
+          onEachFeature: function(feature,layer){
+            layer.on({
+                mouseover: function(event) {
+                  layer = event.target;
+                  layer.setStyle({
+                    fillOpacity: 0.9
+                  });
+                  },
+                mouseout: function(event) {
+                  layer = event.target;
+                  layer.setStyle({
+                    fillOpacity: 0.5
+                  });
+                  },
+                click: function(event){
+                    myMap.fitBounds(event.target.getBounds());             
+            }
+          })  
+              layer.bindPopup(eventtype);           
+          } 
         })
-    });
-    console.log(zones);
-/*
-    d3.json(zones).then(datalatlon => {
-        console.log(datalatlon);
-        datalatlon2 = datalatlon[1].coordinates;
-        console.log(dataslatlon2);
-        datalatlon2.forEach((rowlatlon, k) => {
-            lat = rowlatlon.lat;
-            lon = rowlatlon.lon;
-            coordinates.forEach((rowlatlon2, l) => {
-                lat.push(rowlatlon2);
-                lon.push(rowlatlon2);
-            })
+        d3.json(url2).then(data2=> {
+          console.log(data2);
+            var fireLayer = L.geoJson(data2, {
+              style: function(feature) {
+                return {
+                    color: "red"
+                };
+              }
+          })
+          insertMap(alertLayer,fireLayer)
         })
-        console.log(lat, lon);
+      })
     })
-*/    
-})
-/*
-d3.json(zones).then(datalatlon => {
-    console.log(datalatlon);
-    datalatlon2 = datalatlon[1].coordinates;
-    console.log(dataslatlon2);
-    datalatlon2.forEach((rowlatlon, k) => {
-        lat = rowlatlon.lat;
-        lon = rowlatlon.lon;
-        coordinates.forEach((rowlatlon2, l) => {
-            lat.push(rowlatlon2);
-            lon.push(rowlatlon2);
-        })
-    })
-    console.log(lat, lon);
-})
-*/
-// d3.selectAll("body").append("div").text("test");
+  })
+});
+
